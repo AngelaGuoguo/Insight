2e064b5027bbe5f5c6784ae97fd8da101bbda3e2
yjli <yjlinotlazy@gmail.com>
2016-06-22 15:59:14 -0400

    reformatted

--- generate_lists.py
+++ generate_lists.py
@@ -5,45 +5,43 @@ import pandas as pd
 import util
 from util import load, load_sql
 
-'''
-calculate patients' icu visit frequencies
-inputs
-con: postgres connection
-path_in, path_out: csv containing patient ids, path to save freq
-'''
-def frequency_counts(con,path_in,path_out):
-    patients = pd.read_csv(path_in,usecols=['subject_id'])
-    
+
+# calculate patients' icu visit frequencies
+# inputs:
+# con: postgres connection
+# path_in, path_out: csv containing patient ids, path to save freq
+def frequency_counts(con, path_in, path_out):
+    patients = pd.read_csv(path_in, usecols=['subject_id'])
+
     # generate patient list
     p_list = patients['subject_id'].tolist()
-    
+
     # request icu events
-    icu = load_sql(con,"SUBJECT_ID,INTIME,OUTTIME,LOS","mimiciii.ICUSTAYS")
+    icu = load_sql(con, "SUBJECT_ID,INTIME,OUTTIME,LOS",
+                   "mimiciii.ICUSTAYS")
 
     # only keep relevant patients
     icu = icu[icu['subject_id'].isin(p_list)]
-    
+
     # assign icu frequencies to patients
     icu_counts = icu['subject_id'].value_counts().to_frame()
-    icu_counts['id']=icu_counts.index
-    icu_counts.columns = ['counts','subject_id']
-    
+    icu_counts['id'] = icu_counts.index
+    icu_counts.columns = ['counts', 'subject_id']
+
     # save icu frequencites to file
-    icu_counts.to_csv(path_out,index=False)
-    
+    icu_counts.to_csv(path_out, index=False)
+
     # return list of patients admitted twice
-    twice=icu_counts[icu_counts['counts']==2]
+    twice = icu_counts[icu_counts['counts'] == 2]
     return twice['subject_id'].tolist()
 
-'''
-calculate difference between icu visits
-input: list of patient ids
-output: subject_id, first ICU ID, second ICU ID, diff
-''' 
-def readmission_diff(con,patients):
-    
+
+# calculate difference between icu visits
+# input: list of patient ids
+# output: subject_id, first ICU ID, second ICU ID, diff
+def readmission_diff(con, patients):
+
     # get list of first icu stays
-    
     sql_query = """
     SELECT DISTINCT ON (SUBJECT_ID)
         SUBJECT_ID,ICUSTAY_ID, INTIME, OUTTIME
@@ -52,57 +50,60 @@ def readmission_diff(con,patients):
     ;
     """
 
-    first_icu = pd.read_sql_query(sql_query,con)
-    first_icu = first_icu[first_icu['subject_id'].\
-                  isin(patients)].drop(['intime'],axis=1)
-    
+    first_icu = pd.read_sql_query(sql_query, con)
+    first_icu = first_icu[first_icu['subject_id'].
+                          isin(patients)].drop(['intime'], axis=1)
+
     icu_id = first_icu['icustay_id'].tolist()
-    
+
     # then find the first icu stay not in above list
     # = second icu stay
     query_list = "("+str(icu_id)[1:-1]+")"
-    
+
     sql_query = """
     SELECT DISTINCT ON (SUBJECT_ID)
         SUBJECT_ID,ICUSTAY_ID, INTIME, OUTTIME
     FROM mimiciii.ICUSTAYS
-    WHERE ICUSTAY_ID NOT IN %s 
+    WHERE ICUSTAY_ID NOT IN %s
     ORDER BY SUBJECT_ID, INTIME
-    ; 
+    ;
     """ % query_list
-    
-    second_icu = pd.read_sql_query(sql_query,con)
-    second_icu = second_icu[second_icu['subject_id'].\
-                   isin(patients)].drop(['outtime'],axis=1)
-    
-    readmission = pd.merge(first_icu,second_icu,on='subject_id',how='inner'\
-                       ).sort_values(by=['subject_id'])
 
-    readmission['diff']=(readmission['intime']-readmission['outtime']).dt.days
-    readmission.columns=['subject_id','first_icu',\
-                      'first_out','second_icu','second_in','diff']
+    second_icu = pd.read_sql_query(sql_query, con)
+    second_icu = second_icu[second_icu['subject_id'].
+                            isin(patients)].drop(['outtime'], axis=1)
+
+    readmission = pd.merge(first_icu, second_icu, on='subject_id',
+                           how='inner').sort_values(by=['subject_id'])
+
+    readmission['diff'] = (readmission['intime']-readmission['outtime']).\
+        dt.days
+    readmission.columns = ['subject_id', 'first_icu',
+                           'first_out', 'second_icu', 'second_in', 'diff']
     return readmission
+
 
 # return and save the list of all patients that were adults (age>=15)
 # at time of first admission and stayed in ICU for at least 24 hours
 def extract_adults(con):
     # get list of all patients
-    patients = load_sql(con,"SUBJECT_ID, DOB, DOD, DOD_HOSP, DOD_SSN",\
-               "mimiciii.patients")
-    print 'patients count ',patients.shape[0]
-    
+    patients = load_sql(con, "SUBJECT_ID, DOB, DOD, DOD_HOSP, DOD_SSN",
+                        "mimiciii.patients")
+    print 'patients count ', patients.shape[0]
+
     # consolidate date of death
-    def dod(a,b,c):
-        if b != None:
+    def dod(a, b, c):
+        if b is not None:
             return b
-        elif a != None:
+        elif a is not None:
             return a
         else:
             return c
-    
-    patients['combined_dod'] = patients.apply(lambda row: \
-                 dod(row['dod'], row['dod_hosp'],row['dod_ssn']), axis=1)
-    
+
+    patients['combined_dod'] = patients.apply(
+                lambda row: dod(row['dod'], row['dod_hosp'],
+                                row['dod_ssn']), axis=1)
+
     # get list of first icu stays
     sql_query = """
     SELECT DISTINCT ON (SUBJECT_ID)
@@ -112,40 +113,44 @@ def extract_adults(con):
     ORDER BY SUBJECT_ID, INTIME
     ;
     """
-    first_icu = pd.read_sql_query(sql_query,con)
+    first_icu = pd.read_sql_query(sql_query, con)
     print "first icu stayed >= 24 hours", first_icu.shape[0]
-    
-    icu_patients = pd.merge(patients, first_icu, on=u'subject_id',how='inner')
-    icu_patients['age']=(icu_patients[u'intime']-icu_patients[u'dob']).dt.days/365
-    
+
+    icu_patients = pd.merge(patients, first_icu, on=u'subject_id', how='inner')
+    icu_patients['age'] = (icu_patients[u'intime'] -
+                           icu_patients[u'dob']).dt.days/365
+
     # relax adults to age >=15
-    adults = icu_patients[icu_patients['age']>=15]
-    
-    print "adults admitted and stayed for >= 24 hrs: ",adults.shape[0]
+    adults = icu_patients[icu_patients['age'] >= 15]
+
+    print "adults admitted and stayed for >= 24 hrs: ", adults.shape[0]
     return adults
-    
-# input: 'patients' should be a dataframe with dod and discharge time    
+
+
+# input: 'patients' should be a dataframe with dod and discharge time
 def lived(patients):
     # discharged alive
     # = patients still lived + patients died >=1 day after discharged
     still_lived = patients[patients['combined_dod'].isnull()]
-    
-    dead=patients[~patients['combined_dod'].isnull()].copy()
-    dead['dod']=dead['combined_dod'].apply(util.convert_time)
-    dead=dead[~dead['outtime'].isnull()]
-    dead['outtime']=dead['outtime'].apply(util.convert_time)
-    dead['lived']=(dead['dod']-dead['outtime']).dt.days
-      
-    discharged = (dead[dead['lived']>=1]).drop(['lived'],axis=1)
 
-    combined = pd.concat([still_lived,discharged]).sort_values(by=\
-               ['subject_id']).drop(['dod','combined_dod','outtime'],axis=1)
+    dead = patients[~patients['combined_dod'].isnull()].copy()
+    dead['dod'] = dead['combined_dod'].apply(util.convert_time)
+    dead = dead[~dead['outtime'].isnull()]
+    dead['outtime'] = dead['outtime'].apply(util.convert_time)
+    dead['lived'] = (dead['dod'] - dead['outtime']).dt.days
 
-    combined = combined[['subject_id','hadm_id','icustay_id','age']]
+    discharged = (dead[dead['lived'] >= 1]).drop(['lived'], axis=1)
+
+    combined = pd.concat([still_lived,
+                          discharged]).sort_values(by=['subject_id']).
+    drop(['dod', 'combined_dod', 'outtime'], axis=1)
+
+    combined = combined[['subject_id', 'hadm_id', 'icustay_id', 'age']]
     return combined
 
-# input: postgres connection, list of patients to be filtered    
-def generate_heart_patients(con,patients):
+
+# input: postgres connection, list of patients to be filtered
+def generate_heart_patients(con, patients):
     '''
     will be based on comorbidity label
     1: congestive_heart_failure
@@ -155,7 +160,7 @@ def generate_heart_patients(con,patients):
     5. peripheral_vascular
     6. hypertension
     '''
-    
+
     # get HADM_ID associated with first icu stay
     # since that's what's used by comorbidity
     sql_query = """
@@ -164,10 +169,10 @@ def generate_heart_patients(con,patients):
         FROM MIMICIII.ICUSTAYS
         ORDER BY SUBJECT_ID, INTIME
     """
-    hadms = pd.read_sql_query(sql_query,con)
+    hadms = pd.read_sql_query(sql_query, con)
     hadms = hadms[hadms['subject_id'].isin(patients)]
     hadms_list = set(hadms['hadm_id'].tolist())
-    
+
     # from table elixhauser_ahrq
     sql_query = """
     SELECT SUBJECT_ID, HADM_ID
@@ -178,22 +183,25 @@ def generate_heart_patients(con,patients):
             + hypertension) >= 1
     ;
     """
-    comorbidity = pd.read_sql_query(sql_query,con)
-    
+    comorbidity = pd.read_sql_query(sql_query, con)
+
     # narrow down to first icu stays
     comorbidity = comorbidity[comorbidity['hadm_id'].isin(hadms_list)]
-    merged = pd.merge(comorbidity,hadms,on=['subject_id','hadm_id'],how='inner')
+    merged = pd.merge(comorbidity, hadms,
+                      on=['subject_id', 'hadm_id'], how='inner')
     return merged
 
+
 # show control vs case numbers. To see class imbalance
-def generate_case(path_in,path_out,limit):
+def generate_case(path_in, path_out, limit):
     all_df = pd.read_csv(path_in)
-    case=all_df[all_df['diff']<=limit]
-    
-    case.to_csv(path_out,index=False)
-    print all_df.shape,case.shape
-    
-def generate_icu_id(con,names_path,path_out):
+    case = all_df[all_df['diff'] <= limit]
+
+    case.to_csv(path_out, index=False)
+    print all_df.shape, case.shape
+
+
+def generate_icu_id(con, names_path, path_out):
     # get list of first icu stays
     sql_query = """
     SELECT DISTINCT ON (SUBJECT_ID)
@@ -202,50 +210,54 @@ def generate_icu_id(con,names_path,path_out):
     ORDER BY SUBJECT_ID, INTIME
     ;
     """
-    
-    first_icu = pd.read_sql_query(sql_query,con)
-    names_df=pd.read_csv(names_path,usecols=['subject_id'])
-    name_list=names_df['subject_id'].tolist()
-    icu_list=first_icu[first_icu['subject_id'].isin(name_list)]
-    icu_list['icustay_id'].to_csv(path_out,index=False,header=False)
+
+    first_icu = pd.read_sql_query(sql_query, con)
+    names_df = pd.read_csv(names_path, usecols=['subject_id'])
+    name_list = names_df['subject_id'].tolist()
+    icu_list = first_icu[first_icu['subject_id'].isin(name_list)]
+    icu_list['icustay_id'].to_csv(path_out, index=False, header=False)
+
 
 def main():
     # establish postgresql connection
-    con = psycopg2.connect(database = 'mimic', user = 'mimic', host='localhost'\
-                      , password='mimic')
-    
+    con = psycopg2.connect(database='mimic', user='mimic',
+                           host='localhost', password='mimic')
+
     # 1. extract and export list of adults
     adults = extract_adults(con)
-    adults.to_csv("lists/adults_admitted.csv",index=False,columns=\
-                  ['subject_id','combined_dod','outtime','age'])              
+    adults.to_csv("lists/adults_admitted.csv", index=False,
+                  columns=['subject_id', 'combined_dod', 'outtime', 'age'])
     # ----
-    
+
     # 2. extract patients with cardiovascular conditions
     adults = load("lists/adults_admitted.csv")
     adults_list = set(adults['subject_id'].tolist())
-     
-    heart_patients = generate_heart_patients(con, adults_list)
-    heart_patients.to_csv("lists/heart_patients.csv",index=False,header=True)
-    
-    adults_heart = pd.merge(adults,heart_patients,on=['subject_id'],how='inner')
 
-    # 3. then trim down the list to patients who were discharged alive    
+    heart_patients = generate_heart_patients(con, adults_list)
+    heart_patients.to_csv("lists/heart_patients.csv", index=False, header=True)
+
+    adults_heart = pd.merge(adults, heart_patients,
+                            on=['subject_id'], how='inner')
+
+    # 3. then trim down the list to patients who were discharged alive
     discharged = lived(adults_heart)
-    discharged.to_csv("lists/adults_heart_discharged.csv",index=False)
-    
-    #discharged = load("lists/adults_heart_discharged.csv")
-    
+    discharged.to_csv("lists/adults_heart_discharged.csv", index=False)
+
+    # discharged = load("lists/adults_heart_discharged.csv")
+
     # 4. from those discharged patients, find the ones with a second
     # admission and calculate the data difference between first and second
     readmitted = readmission_diff(con, discharged['subject_id'].tolist())
-    readmitted.to_csv("lists/readmission_diff.csv",index=False)
-    
+    readmitted.to_csv("lists/readmission_diff.csv", index=False)
+
     # 5. generate icu id's of first visits (could be used for awk)
-    generate_icu_id(con,"lists/readmission_diff.csv","lists/first_icu_list.txt")
-  
+    generate_icu_id(con, "lists/readmission_diff.csv",
+                    "lists/first_icu_list.txt")
+
     # print "now run: "
-    # print "awk -F, 'FNR==NR{k[$1]=1;next;} FNR==1 || k[$4]' filter_list file.csv"
+    # print "awk -F, 'FNR==NR{k[$1]=1;next;} FNR==1 || k[$4]' list file"
     # change $4 to $[columne number, 1-based] as necessary
-    
+
+
 if __name__ == '__main__':
     main()
--- notes_nlp.py
+++ notes_nlp.py
@@ -9,6 +9,7 @@ import math
 import gensim
 from gensim import corpora, models
 
+
 '''
 !!! INCOMPLETE !!!
 Though I completed the most hardware consuming step: lda,
@@ -16,16 +17,18 @@ I decided to stop here and didn't continue feature generation
 and such, for the sake of ROI
 '''
 
+
 # retrive notes from postseq
 def retrieve_notes_sql():
-    con = psycopg2.connect(database = 'mimic', user = 'mimic', host='localhost'\
-                      , password='mimic')
+    con = psycopg2.connect(database='mimic', user='mimic', host='localhost',
+                           password='mimic')
 
     # load list of first icus
-    icu_list = load("notes/adults_heart_discharged.csv",cols=['subject_id','hadm_id'])
-    
+    icu_list = load("notes/adults_heart_discharged.csv",
+                    cols=['subject_id', 'hadm_id'])
+
     adm_id = icu_list['hadm_id'].tolist()
-   
+
     # then find the first icu stay not in above list
     query_list = "("+str(adm_id)[1:-1]+")"
 
@@ -37,27 +40,35 @@ def retrieve_notes_sql():
         ORDER BY SUBJECT_ID, chartdate, charttime
         ;
         """ % query_list
-    notes = pd.read_sql_query(sql_query,con)
-    notes.to_csv("notes/notes.csv",index=False)
-    #print notes.shape
+    notes = pd.read_sql_query(sql_query, con)
+    notes.to_csv("notes/notes.csv", index=False)
+    # print notes.shape
     return True
 
-# start reference: http://stevenloria.com/finding-important-words-in-a-document-using-tf-idf/
+
+# start reference: http://stevenloria.com/finding-important-
+# words-in-a-document-using-tf-idf/
 def tf(word, doc):
+    if len(doc) == 0:
+        return 0
     return float(doc.count(word)) / len(doc)
+
 
 def df(word, docs):
     return sum(1 for doc in docs if word in doc)
+
 
 # drop the log term. No need for LDA. add 1 to denominater
 # to avoid infinity
 def idf(word, docs):
     return float(len(docs)) / (1 + df(word, docs))
-#print idf('a',docs),idf('b',docs)
+# print idf('a',docs),idf('b',docs)
+
 
 def tfidf(word, doc, docs):
     return tf(word, doc) * idf(word, docs)
 # end reference
+
 
 def tokenize_notes(text):
     tokenizer = RegexpTokenizer(r'\w+')
@@ -65,21 +76,22 @@ def tokenize_notes(text):
     tokens = tokenizer.tokenize(raw)
     # stop words
     stop = get_stop_words('en')
-    stopped = [i for i in tokens if not i in stop]
+    stopped = [i for i in tokens if i not in stop]
     # stemmer: No! stemmer is a bad choice for doc notes
-    #stemmer = PorterStemmer()
-    #stemed = [stemmer.stem(i) for i in stopped]
+    # stemmer = PorterStemmer()
+    # stemed = [stemmer.stem(i) for i in stopped]
     return stopped
 
-def tokenize_file(path_in,path_out):
-    f_out = open(path_out,'w')
-    counter=0
-    
-    with open(path_in,'r') as f:
+
+def tokenize_file(path_in, path_out):
+    f_out = open(path_out, 'w')
+    counter = 0
+
+    with open(path_in, 'r') as f:
         for line in f:
-            if counter % 10000 ==0:
+            if counter % 10000 == 0:
                 print counter
-            counter+=1
+            counter += 1
             s = line.split(",")
             words = tokenize_notes(s[5])
             for word in words:
@@ -87,7 +99,7 @@ def tokenize_file(path_in,path_out):
             f_out.write('\n')
     f_out.close()
     f.close()
-    return 0    
+    return 0
 
 # I really need to trim down the texts to do lda
 # main issue: such a big dictionary wont fit into
@@ -96,28 +108,30 @@ def tokenize_file(path_in,path_out):
 # OK, let's just get rid of some samples for efficiency...
 #
 
+
 # well, try lda first anyways
-def generate_top_words(path_in,path_out,limit=500):
+def generate_top_words(path_in, path_out, limit=500):
     # get collection of docs from path_in
-    f_out = open(path_out,'w')
+    f_out = open(path_out, 'w')
     docs = []
-    #print docs,type(docs)
+
     counter = 0
-    with open(path_in,'r') as f:
+    with open(path_in, 'r') as f:
         for line in f:
-            if counter % 10000 ==0:
+            if counter % 10000 == 0:
                 print counter
-            counter+=1
+            counter += 1
             s = line[:-2].split(' ')
             docs.append(s)
     f.close()
-    # ref: https://rstudio-pubs-static.s3.amazonaws.com/79360_850b2a69980c4488b1db95987a24867a.html
+    # ref: https://rstudio-pubs-static.s3.amazonaws.com/
+    # 79360_850b2a69980c4488b1db95987a24867a.html
     dictionary = corpora.Dictionary(docs)
     corpus = [dictionary.doc2bow(text) for text in docs]
-    ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=20, \
-                                               id2word = dictionary)
+    ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=20,
+                                               id2word=dictionary)
     ldamodel.save("notes/ldamodel_all")
-    topics = ldamodel.show_topics(num_topics=20,num_words=20)
+    topics = ldamodel.show_topics(num_topics=20, num_words=20)
     for topic in topics:
         f_out.write(str(topic))
         f_out.write('\n')
@@ -125,30 +139,31 @@ def generate_top_words(path_in,path_out,limit=500):
     return 0
 
 
-def build_dict_tokens(path_in,path_out):
+def build_dict_tokens(path_in, path_out):
     # get a set
-    counter=0
+    counter = 0
     unique = set()
-    with open(path_in,'r') as f:
+    with open(path_in, 'r') as f:
         for line in f:
-            if counter % 10000 ==0:
+            if counter % 10000 == 0:
                 print counter
                 print unique
-            counter+=1
+            counter += 1
             words = line.split(' ')
             for w in words:
                 unique.add(w)
     f.close()
-    zipped=zip(unique,range(len(unique)))
-    with open(path_out,'w') as fout:
+    zipped = zip(unique, range(len(unique)))
+    with open(path_out, 'w') as fout:
         fout.write('word,indx\n')
-        for x,y in zipped:
+        for x, y in zipped:
             string = x+','+str(y)+'\n'
             fout.write(string)
     fout.close()
-    
+
+
 def main():
-    #retrieve_notes_sql()
+    # retrieve_notes_sql()
     # tokenize_file("notes/notes_single_line.csv",'notes/tokenized_all.txt',)
     # then sed '1d' file_out > tmp; mv tmp file_out to remove first header line
 
@@ -156,9 +171,10 @@ def main():
     # build_dict_tokens('notes/tokenized_all.txt','notes/unique_words.txt')
 
     # generate top words
-    generate_top_words('notes/tokenized_all.txt','notes/top_words_all_notes.txt')
-    
+    generate_top_words('notes/tokenized_all.txt',
+                       'notes/top_words_all_notes.txt')
     return 0
+
 
 if __name__ == "__main__":
     main()
